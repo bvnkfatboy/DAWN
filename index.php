6848a7e0cd740f9b928a3fdfc8801e6b0a6607ed
Theerapong Bank <theerapong.bvnk@gmail.com>
2022-01-14 12:04:32 +0700

    update

---
+++ index.php
@@ -0,0 +1,35 @@
+
+้<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php $output = '<title>%TITLE%</title>'; ?>
+</head>
+<body>
+<?php 
+
+$current_page = isset($_GET['page']) ? $_GET['page'] : 'home' ;
+
+
+switch ($current_page) {
+    case ('home'):
+        include_once 'homepage.php';
+        $title = "หน้าแรก - DAWN (Coffee & Bar)";
+        $output = str_replace('%TITLE%', $title, $output);
+        echo $output;
+        break;
+    default:
+        include_once 'error.php';
+        $title = "ไม่มีหน้านี้ - DAWN (Coffee & Bar)";
+        $output = str_replace('%TITLE%', $title, $output);
+        echo $output;
+}
+
+
+?>
+</body>
+</html>
+
+
