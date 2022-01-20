124950ef55677c732ecf7b9da72cf5c6d99ac3b9
Theerapong Bank <theerapong.bvnk@gmail.com>
2022-01-20 12:54:27 +0700

    update

--- include/navbar.php
+++ include/navbar.php
@@ -1,36 +1,97 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<style>
+  .ul-acc-nav {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    background-color: #fbc531;
+  }
+
+  .ul-acc-nav li {
+    float: left;
+  }
+  .ul-acc-nav p {
+    margin:0;
+    padding: 0;
+    margin-top: 14px;
+  }
+  .ul-acc-nav li a,p {
+    display: block;
+    color: #2f3640;
+    text-align: center;
+    padding: 10px 5px;
+    text-decoration: none;
+  }
+
+
+</style>
+
+
+<div class="acc-nav">
+
+<ul class="ul-acc-nav">
+  <div class="container">
+    
+  <li><a href="#"> <i class="fab fa-instagram"></i> dawn.coffee</a></li>
+  
+  <li style="float:right"><a href="">สมัครสมาชิก</a></li>
+  <li style="float:right" ><a style="color: #2f3640;">|</a></li>
+  <li style="float:right" ><a href="">เข้าสู่ระบบ</a></li>
+  <li style="float:right" ><a style="color: #2f3640;"><i class="fas fa-user-alt"></i></a></li>
+  
+
+  
+  </div>
+</ul>
+
+</div>
+
+<style>
+  .bg-c-light{
+    background-color: white;
+  }
+
+  .navbar-nav a{
+    color: black !important;
+  }
+  .navbar-nav a:hover{
+    color: rgb(150, 150, 150) !important;
+  }
+</style>
+
+
+<nav class="navbar navbar-expand-md navbar-light bg-c-light">
+  <div class="container">
   <a class="navbar-brand" href="#">
+    <img src="dist/img/logo_dawn.png" alt="logo" width="50px">
   </a>
+
+  
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
+        <a class="nav-link" href="#">หน้าแรก</a>
       </li>
+
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
+          รายการสินค้าทั้งหมด
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
+          <a class="dropdown-item" href="#">สินค้า 1</a>
+          <a class="dropdown-item" href="#">สินค้า 2</a>
+
         </div>
       </li>
       <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
+        <a class="nav-link" href="#">เกี่ยวกับ</a>
       </li>
     </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
   </div>
+  </div>
+
 </nav>
--- index.php
+++ index.php
@@ -4,16 +4,21 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
     <?php $output = '<title>%TITLE%</title>'; ?>
-    <!-- <link rel="stylesheet" href="dist/css/reset.css"> -->
+
+
+    <!-- font  -->
+    <link rel="stylesheet" href="dist/css/font.css">
+    <link rel="stylesheet" href="dist/css/fontawesome.css">
+
+    <link rel="stylesheet" href="dist/css/reset.css">
     <link rel="icon" href="dist/img/icontitle.png" type="image/png" >
-
-
-
+    
     <link rel="stylesheet" href="dist/bootstrap/bootstrap.css">
 </head>
 <body>
+
+
 <?php 
 $current_page = isset($_GET['page']) ? $_GET['page'] : 'home' ;
 
